d43dbcac9b18e6b7c1478cbfb1efef0de8daad1a
fcpage <colmanfp@gmail.com>
2026-05-23 18:54:17 -0400

    Done Forms
    Should take us to A1Q9

--- general/php/request_access.php
+++ general/php/request_access.php
@@ -12,7 +12,16 @@ $submitted = !empty($_POST);
 		<p>Form Submitted? <?php echo (int) $submitted; ?> </p>
 		<p>Access Request:</p>
 		<ul>
-			<li><b>Details</b> <?php echo $_POST['access']; ?></li>
+			<li><b>First Name</b> <?php echo $_POST['firstname']; ?></li>
+			<li><b>Last Name</b> <?php echo $_POST['lastname']; ?></li>
+			<li><b>Email</b> <?php echo $_POST['email']; ?></li>
+			<li><b>Website</b> <?php echo $_POST['url']; ?></li>
+			<li><b>Persona</b> <?php echo $_POST['person']; ?></li>
+			<li><b>Involvement</b> <?php echo $_POST['involvement']; ?></li>
+			<li><b>Reason</b> <?php echo $_POST['reason']; ?></li>
+			<li><b>Details</b> <?php echo $_POST['details']; ?></li>
+			<li><b>Deadline</b> <?php echo $_POST['deadline']; ?></li>
+			<li><b>Feedback</b> <?php echo $_POST['good_job']; ?></li>
 		</ul>
 	</body>
 </html>
