8379ffb7c7d689f8735334810afef4267f8460f9
fcpage <colmanfp@gmail.com>
2026-05-31 23:39:10 -0400

    Condensation!

--- general/php/gui.php
+++ general/php/gui.php
@@ -8,22 +8,14 @@
     <body>
         <?php $last=$_POST["called_from"]; ?>
         <?php $call=$_POST["called_to"]; ?>
-        <?php
-        if( $call  > $last) {
-            $direction = "UP";
-        }
-        else if ($call < $last ){
-            $direction = "DOWN";
-        }
-        else{
-            $direction = "HOLD";
-        } ?>
+        <?php if($call  > $last) {$direction = "UP";}
+        else if ($call < $last ) {$direction = "DOWN";}
+        else {$direction = "HOLD";} ?>
         <form method="post" id="buttons" target="_self" onclick="buttons.submit()">
-            <label><input type="radio" name="called_to" value="3" required />Floor 3</label><br/>
-            <label><input type="radio" name="called_to" value="2" required />Floor 2</label><br/>
-            <label><input type="radio" name="called_to" value="1" required />Floor 1</label><br/>
-            <input type="hidden" name="called_from" value="<?php echo $call; ?>" required />
-        </form><br/>
+            <label><input type="radio" name="called_to" value=11 required />Floor 3</label><br/>
+            <label><input type="radio" name="called_to" value=10 required />Floor 2</label><br/>
+            <label><input type="radio" name="called_to" value=01 required />Floor 1</label><br/>
+            <input type="hidden" name="called_from" value="<?php echo $call; ?>" required /></form><br/>
         <b>Called to: </b> <?php echo $call; ?><br/>
         <b>Called from: </b> <?php echo $last; ?><br/>
         <b>Direction: </b> <?php echo $direction; ?>
