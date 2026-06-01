ef66d89d2355e0aea8ccdce341a3a5484539ca4d
fcpage <colmanfp@gmail.com>
2026-05-31 22:49:54 -0400

    Condensed formatting

--- general/php/gui.php
+++ general/php/gui.php
@@ -8,35 +8,24 @@
     <body>
         <?php $last=$_POST["called_from"]; ?>
         <?php $call=$_POST["called_to"]; ?>
-
-
-
+        <?php
+        if( $call  > $last) {
+            $direction = "UP";
+        }
+        else if ($call < $last ){
+            $direction = "DOWN";
+        }
+        else{
+            $direction = "HOLD";
+        } ?>
         <form method="post" id="buttons" target="_self" onclick="buttons.submit()">
             <label><input type="radio" name="called_to" value="3" required />Floor 3</label><br/>
             <label><input type="radio" name="called_to" value="2" required />Floor 2</label><br/>
             <label><input type="radio" name="called_to" value="1" required />Floor 1</label><br/>
             <input type="hidden" name="called_from" value="<?php echo $call; ?>" required />
         </form><br/>
-
         <b>Called to: </b> <?php echo $call; ?><br/>
         <b>Called from: </b> <?php echo $last; ?><br/>
-        <?php
-        if( $call  > $last) {
-            $direction = "UP";
-        }
-
-        else if ($call < $last ){
-            $direction = "DOWN";
-        }
-
-        else{
-            $direction = "HOLD";
-        }
-        ?>
         <b>Direction: </b> <?php echo $direction; ?>
-
-
-
-
     </body>
 </html>
