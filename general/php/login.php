33fe9029cb4a42bfab8c3d4db7140bf9dd01f733
fcpage <colmanfp@gmail.com>
2026-06-04 16:01:22 -0400

    Finished Access Request file write and report

--- general/php/login.php
+++ general/php/login.php
@@ -1,5 +1,5 @@
 <?php $username = $_POST["username"]; $password = $_POST["password"]; $result = 0; //Get login credentials, defaulting to lockout
-$file = fopen("../documents/logins.txt", "r",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //open valid login list
+$file = fopen("../documents/requests/login/logins.txt", "r",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //open valid login list
 while(!feof($file) && !$result) {$contents = fgets($file); //check each line until the file is through or the login is verified
     str_contains($contents, "username:$username-password:$password") ? ($result = 1) : ($result = 0);} //if the login key is found, make $result true, if not, make $result false
 fclose($file); $result ? header("Location: ../html/.gui.html") : header("Location: ../html/lockout.html"); //close the login list and redirect to end page
--- general/php/request_access.php
+++ general/php/request_access.php
@@ -1,15 +1,37 @@
-<?php $date = date("Y-m-d"); $time = date("H:i:s");
-$key[0]='fir'; $key[1] = 'las'; $key[2] = 'ema'; $key[3] = 'url'; $key[4] = 'per';
-$key[5] = 'rea'; $key[6] = 'det'; $key[7] = 'dea'; $key[8] = 'goo'; $key[9] = 'inv';
-$data['fir']= $_POST['firstname']; $data['las']= $_POST['lastname']; $data['ema'] = $_POST['email'];
-$data['url'] = $_POST['url']; $data['per'] = $_POST['person']; $data['rea'] = $_POST['reason'];
-$data['det'] = $_POST['details']; $data['dea'] = $_POST['deadline']; $data['goo'] = $_POST['good_job'];
-foreach (array_filter($_POST['involvement']) as $inv) {$data['inv'][]=$inv;}
-$desc['fir'] = "First Name: "; $desc['las'] = "Last Name: "; $desc['ema'] = "Email: "; $desc['url'] = "Website: ";
-$desc['per'] = "Person: "; $desc['rea'] = "Reason: "; $desc['det'] = "Details: "; $desc['dea'] = "Deadline: ";
-$desc['goo'] = "Feedback: "; $desc['inv'] = "Involvement: "; $contents = "$date\n$time\n\nAccess Request:\n\n";
-for ($i=0; $i<8; $i++) {$contents = "$contents.$desc[$i].$data[$i]\n";} $contents = $contents.$desc['inv'];
-foreach ($data['inv'] as $inv) {$contents = "$contents.$inv\n";} $contents = $contents."\nEOF";
-$file = fopen("../documents/requests/access.php", 'w');
-fwrite($file, $contents);
-fclose($file)?>
+<?php $date = date("Y-m-d"); $time = date("H-i-s"); $key = [0=>"."]; $desc = [0=>"."];
+array_push($key,'firstname', 'lastname', 'email', 'url', 'person', 'reason', 'details', 'deadline', 'good_job');
+array_push($desc, "First Name: ", "Last Name: ", "Email: ", "Website: ", "Person: ", "Reason: ", "Details: ", "Deadline: ", "Feedback: ", "Involvement: ");
+$contents = "Date: $date\nTime: $time\n\nAccess Request:\n\n"; $contents .= $desc[8];
+foreach (array_filter($_POST['involvement']) as $i) {$contents = $contents .= $i;}; $contents .= "\n";
+for($i=1; $i < 10; $i++) {$contents = $contents .= $desc[$i] .= $_POST[$key[$i]] .= "\n";}
+file_put_contents("../documents/requests/access/access_request_{$date}_{$time}.txt", $contents); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Review Access Request</title>
+    <meta name="description" content="This is the access request handler page for our project" />
+    <meta http-equiv="author" content="Fergus Page" />
+    <meta name="robots" content="noindex nofollow" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <link rel="stylesheet" href="../css/general_style.css" type="text/css">
+</head>
+
+<body>
+    <h1>Access Form Handler Page</h1>
+    <p>Your request has been submitted for consideration.<br/>Please review the details of your request below.</p>
+    <h3>Access Request:</h3>
+    <ul>
+        <li><b>First Name</b> <?php echo $_POST['firstname']; ?></li>
+        <li><b>Last Name</b> <?php echo $_POST['lastname']; ?></li>
+        <li><b>Email</b> <?php echo $_POST['email']; ?></li>
+        <li><b>Website</b> <?php echo $_POST['url']; ?></li>
+        <li><b>Persona</b> <?php echo $_POST['person']; ?></li>
+        <li><b>Involvement</b> <?php foreach (array_filter($_POST['involvement']) as $i) {echo $i;}?></li>
+        <li><b>Reason</b> <?php echo $_POST['reason']; ?></li>
+        <li><b>Details</b> <?php echo $_POST['details']; ?></li>
+        <li><b>Deadline</b> <?php echo $_POST['deadline']; ?></li>
+        <li><b>Feedback</b> <?php echo $_POST['good_job']; ?></li>
+    </ul>
+</body>
+</html>
