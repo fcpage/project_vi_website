c1b8f42d33af86bfb7100485e216d7fa0fd4e4ba
fcpage <colmanfp@gmail.com>
2026-06-05 22:41:00 -0400

    HTML and PHP comments

--- general/php/login.php
+++ general/php/login.php
@@ -1,5 +1,14 @@
-<?php $username = $_POST["username"]; $password = $_POST["password"];$result = 0; //Get login credentials, defaulting to lockout
+<?php
+$username = $_POST["username"]; //get supposed "username"
+$password = $_POST["password"]; //what is the password?
+$result = 0; //default to lockout
 $file = fopen("../documents/requests/login/logins.txt", "r",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //open valid login list
-while(!feof($file) && !$result) {$contents = fgets($file); //check each line until the file is through or the login is verified
-    str_contains($contents, "username:$username-password:$password") ? ($result = 1) : ($result = 0);} //if the login key is found, make $result true, if not, make $result false
-fclose($file); $result ? header("Location: ../html/.gui.html") : header("Location: ../html/lockout.html"); //close the login list and redirect to end page
+
+while(!feof($file) && !$result) {   //while not at the end of the login credential register and no match has been found
+    $contents = fgets($file); //check each line until the file is through or the login is verified
+    str_contains($contents, "username:$username-password:$password") ? ($result = 1) : ($result = 0);//if the login key is found, make $result true, if not, make $result false
+}
+
+fclose($file);
+$result ? header("Location: ../html/.gui.html") : header("Location: ../html/lockout.html"); //close the login list and redirect to end page
+//closing php tag is redundant apparently?
--- general/php/request_access.php
+++ general/php/request_access.php
@@ -1,10 +1,25 @@
-<?php $date = date("Y-m-d"); $time = date("H-i-s"); $key = [0=>"."]; $desc = [0=>"."];
-array_push($key,'firstname', 'lastname', 'email', 'url', 'person', 'reason', 'details', 'deadline', 'good_job');
-array_push($desc, "First Name: ", "Last Name: ", "Email: ", "Website: ", "Person: ", "Reason: ", "Details: ", "Deadline: ", "Feedback: ", "Involvement: ");
-$contents = "Date: $date\nTime: $time\n\nAccess Request:\n\n"; $contents .= $desc[8];
-foreach (array_filter($_POST['involvement']) as $i) {$contents = $contents .= $i;}; $contents .= "\n";
-for($i=1; $i < 10; $i++) {$contents = $contents .= $desc[$i] .= $_POST[$key[$i]] .= "\n";}
-file_put_contents("../documents/requests/access/access_request_{$date}_{$time}.txt", $contents); ?>
+<?php
+$date = date("Y-m-d");  //what day is it???
+$time = date("H-i-s");  //do you have the time, to listen to me whine?
+$key = [0=>"."];    //initialize the key array, non-null
+$desc = [0=>"."];   //initialize the description array, non-null
+array_push($key,'firstname', 'lastname', 'email', 'url', 'person', 'reason', 'details', 'deadline', 'good_job');    //load the keys
+array_push($desc, "First Name: ", "Last Name: ", "Email: ", "Website: ", "Person: ", "Reason: ", "Details: ", "Deadline: ", "Feedback: ", "Involvement: "); //load the descriptions
+$contents = "Date: $date\nTime: $time\n\nAccess Request:\n\n";  //build the array header
+$contents .= $desc[8];  //involvement description
+
+foreach (array_filter($_POST['involvement']) as $i) { //for every checked element
+    $contents = $contents .= $i;    //add it to the data conglomerate
+}
+
+$contents .= "\n";  //new line!
+
+for($i=1; $i < 10; $i++) {  //for every key
+    $contents = $contents .= $desc[$i] .= $_POST[$key[$i]] .= "\n"; //append the descriptions and values
+}
+
+file_put_contents("../documents/requests/access/access_request_{$date}_{$time}.txt", $contents);    //write the log file and give it a unique file name
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,11 +31,13 @@ file_put_contents("../documents/requests/access/access_request_{$date}_{$time}.t
     <meta http-equiv="pragma" content="no-cache" />
     <link rel="stylesheet" href="../css/general_style.css" type="text/css">
 </head>
+
 <body>
     <a href="../../index.html">Home</a><br/>
-    <h1>Access Form Handler Page</h1>
-    <p>Your request has been submitted for consideration.<br/>Please review the details of your request below.</p>
-    <h3>Access Request:</h3>
+    <h1>Access Form Handler Page</h1>   <!-- this bit is the onscreen review -->
+    <p>Your request has been submitted for consideration.<br/>Please review the details of your request below.</p>  <!-- some small talk -->
+
+    <h3>Access Request:</h3>    <!-- here are the details -->
     <ul>
         <li><b>First Name</b> <?php echo $_POST['firstname']; ?></li>
         <li><b>Last Name</b> <?php echo $_POST['lastname']; ?></li>
@@ -33,5 +50,6 @@ file_put_contents("../documents/requests/access/access_request_{$date}_{$time}.t
         <li><b>Deadline</b> <?php echo $_POST['deadline']; ?></li>
         <li><b>Feedback</b> <?php echo $_POST['good_job']; ?></li>
     </ul>
+
 </body>
 </html>
