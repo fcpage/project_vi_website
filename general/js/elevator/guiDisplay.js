b63130bdf5423c84c3b5400dc8691f539defb21c
fcpage <colmanfp@gmail.com>
2026-07-16 01:25:37 -0400

    Reformatted Elevator GUI
    Changed layout in gui_style.css.
    Slight edits to guiDisplay.js to allow for graceful usage in other modules.
    started basic guiBrain.js to deal with control functions.

--- general/js/elevator/guiDisplay.js
+++ general/js/elevator/guiDisplay.js
@@ -1,30 +1,25 @@
-let elements = [];
+const prefix = "../../resources/references/imgs/gui/";    //base relative url prefix
+const lit = "lit.png";  //illuminated suffix
+const unlit = "unlit.png";  //de-luminated suffix
+export let elements = [];  //a place to hang my hat
+const elementNames = ['REQUEST', 'CURRENT', 'DIRECTION', 'DOORS', 'CF3', 'CF2', 'CF1', 'F3D', 'F2U', 'F2D', 'F1U']; //all the names
+const elementIDs = ['REQUEST', 'CURRENT', 'DIRECTION', 'DOORS', '3', '2', '1', 'down', 'up', 'down', 'up']; //all the IDs
+class guiElement {  //a class to keep all the elements consistent, and a method to modify their contents
+    constructor(name) {this.name = name;}   //build an object and name it
 
-const elementNames = ['REQUEST', 'CURRENT', 'DIRECTION', 'DOORS', 'CF3', 'CF2', 'CF1', 'F3D', 'F2U', 'F2D', 'F1U'];
-
-const elementIDs = ['REQUEST', 'CURRENT', 'DIRECTION', 'DOORS', '3', '2', '1', 'down', 'up', 'down', 'up'];
-class guiElement {
-    constructor(name) {     //build an object
-        this.name = name;   //name the element
-    }
-
-   modify(element, id) { //id is for text input
-        if (!id) {console.log('error');
-        } else if (id.length < 5) {
-            if (element.state === false) {element.handle.src = '../../resources/references/imgs/gui/' + element.id + 'lit.png';  //off->on
-            } else {element.handle.src = '../../resources/references/imgs/gui/' + element.id + 'unlit.png';                      //on->off
-            } element.state = !element.state;
-        } else {element.id = element.handle.innerHTML = element.id;}
-    }
+   modify(element, id) {if (!id) {console.log('error'); //id is also used for text input
+        } else if (id.length < 5) {if (element.state === false) {element.handle.src = prefix + element.id + lit;  //off->on
+            } else {element.handle.src = prefix + element.id + unlit;} element.state = !element.state; //toggle   //on->off
+        } else {element.id = element.handle.innerHTML = element.id;    //set gui status report text
+   } console.log("Name: " + element.name + " - ID: " + element.id + " - Status: " + element.state);}
 }
 
-function initElements() { //initialize all the elements
-        console.log("GUI initializing"); //report
+export function initElements() {console.log("GUI initializing"); //start report     //initialize all the elements
     for (let i = 0; i < elementNames.length; i++) { //for each element
         elements[i] = new guiElement(elementNames[i]); //create and name
-        elements[i].state = false;
-        elements[i].id = elementIDs[i];
-        elements[i].handle = document.getElementById(elementNames[i]);
+        elements[i].state = false;  //initialize off
+        elements[i].id = elementIDs[i]; //ID, please
+        elements[i].handle = document.getElementById(elementNames[i]);  //that's where I left my keys
         elements[i].handle.addEventListener('click', () => {elements[i].modify(elements[i], elements[i].id)});    //add a click listener
-    }console.log("Initialization complete"); //report
-} initElements();
+    }console.log("Initialization complete");    //end report
+    return elements;}  //and return
