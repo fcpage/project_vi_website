91d50c3608b929cf266100d779f2df5511352424
fcpage <Colmanfp@gmail.com>
2026-07-27 17:47:22 -0400

    Database reads
    Database reads are done. Needs formatting and integration, but the gui interacts with the database

--- general/php/elevator/login.php
+++ general/php/elevator/login.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . "/session.php";
+require_once __DIR__ . "/Session.php";
+require_once __DIR__ . "/Database.php";
+$row[1] = 1;
 $log = null; //initialize a null variable for attempt logging
 $contents = null; //initialize null file contents variable (global)
 $authentication = null; //initialize null authentication variable, default to lockout (global)
@@ -8,7 +10,7 @@ $username = $_POST["username"]; //get supposed "username"
 $password = $_POST["password"]; //what is the password?
 
 //Authentication
-$file = fopen("../../resources/requests/login/logins.txt", "r",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //open valid login list
+/*$file = fopen("../../resources/requests/login/logins.txt", "r",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //open valid login list
 while(!feof($file) && !$authentication) {   //while not at the end of the login credential register and no match has been found
     $contents = fgets($file); //check each line until the file is through or the login is verified
     str_contains($contents, "username:$username-password:$password") ? ($authentication = "valid") : ($result = null);}//if the authentication key found, make $authentication valid, or else null
@@ -23,21 +25,35 @@ if (str_contains($contents, "-auth:dev")){ //if auth key is dev
     $authorization = "run"; //assign authorization key if found
 } elseif (str_contains($contents, "-auth:admin")){ //else if auth key is run
     $authorization = "admin"; //assign authorization key if found
-} else {$authorization = null;} //else if no authorization, invalid authorization key
+} else {$authorization = null;} //else if no authorization, invalid authorization key*/
+
+$logins = new Database("loginRegistry");
+$sql[] = json_decode($logins->readEntry(null, -1));
+echo $sql[1];
+foreach ($sql as $entry) {
+    if (($entry["username"] == $username) && ($entry["password"] == $password)) {
+        echo $entry["username"];
+        $this->authorization = $row["authorization"];
+        $this->username = $row["username"];
+        $this->password = $row["password"];
+        $this->authentication = "valid";
+        break;
+    }
+}
 
 //Logging
 $log = "Date:" . date("Y-m-d");", Time:" . date("H:i:s");  //log the login attempt date and time
-$log .= "\nUsername: " . $username . "\n" ; //log the entered username
-($authentication != "valid") ? ($log .= "Authentication: " . "invalid" . "\n") : ($log .= "Authentication: " . "valid" . "\n"); //log authentication status
-($authorization == null) ? ($log .= "Authorization: " . "invalid" . "\n\n") : ($log .= "Authorization: " . $authorization . "\n\n"); //log authorization credentials
+$log .= "\nUsername: " . $this->username . "\n" ; //log the entered username
+($this->authentication != "valid") ? ($log .= "Authentication: " . "invalid" . "\n") : ($log .= "Authentication: " . "valid" . "\n"); //log authentication status
+($this->authorization == null) ? ($log .= "Authorization: " . "invalid" . "\n\n") : ($log .= "Authorization: " . $this->authorization . "\n\n"); //log authorization credentials
 file_put_contents("../../resources/requests/login/login_attempts.txt", $log, FILE_APPEND); //log the login attempt
 
 //Redirection
-if ($authentication && $authorization) {    //if authentication is valid and there is a valid authorization key
+if ($this->authentication && $this->authorization) {    //if authentication is valid and there is a valid authorization key
     $session = new Session();
     $session->setSession("login", "true");
-    $session->setSession("username", $username);
-    $session->setSession("authorization", $authorization);
+    $session->setSession("username", $this->username);
+    $session->setSession("authorization", $this->authorization);
     setcookie("login", "true", [
         'expires' => "86400",
         'path' => "/",
